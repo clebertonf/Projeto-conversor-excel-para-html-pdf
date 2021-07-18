d587805d816b2ee00886412e36eebda78feda872
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-18 17:16:35 -0300

    Class table finalizada

---
+++ classes/Table.js
@@ -0,0 +1,9 @@
+class CreateClass {
+    constructor(data) {
+        this.header = data[0];
+        data.shift();
+        this.rows = data;
+    }
+}
+
+module.exports = CreateClass;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const Reader = require('./classes/Reader');
 const processFile = require('./classes/ProcessFile');
+const CreateTable = require('./classes/Table');
 
 const L1 = new Reader();
 
 async function main () {
     const data = await L1.read('./pessoas.csv');
     const  processData = processFile.Process(data);
+
+    const Table = new CreateTable(processData);
+
+    console.log(Table.rows);
 }
 
 main();
