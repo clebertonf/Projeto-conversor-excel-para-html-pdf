f6d8e4d619f869933605ae3e92553574193ffc04
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-18 16:22:49 -0300

    Classe de processamento

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const Reader = require('./Reader');
+const Reader = require('./classes/Reader');
 
 const L1 = new Reader();
 
 async function main () {
     const resp = await L1.read('./pessoas.csv');
-    console.log(resp);
 }
 
 main();
