ce6ed803e8f9f94c12723a6f769ae768bc163d1d
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-19 14:23:06 -0300

    Instalando multer

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('./public'));
