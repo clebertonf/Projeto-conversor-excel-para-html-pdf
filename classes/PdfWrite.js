57cd176aa3284aa94d16540866f1c0f934ae9ca8
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-19 12:06:25 -0300

    Finalizando classe pdf

--- classes/PdfWrite.js
+++ classes/PdfWrite.js
@@ -2,7 +2,8 @@ const pdf = require('html-pdf');
 
 class PdfWriter {
     static Writer (fileName, html) {
-
+        pdf.create(html, {})
+          .toFile(fileName, (err, _resp) => err ? console.error(err) : true);
     }
 }
 
