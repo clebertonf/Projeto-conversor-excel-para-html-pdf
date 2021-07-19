b0841c29a8c548870a2279a50998aea1c74c8902
Cleberton Francisco <clebertonfgc@gmail.com>
2021-07-19 12:26:51 -0300

    Criando estrutura ejs

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-const Reader = require('./classes/Reader');
-const processFile = require('./classes/ProcessFile');
-const CreateTable = require('./classes/Table');
-const HTMLparser = require('./classes/HTMLpaser');
-const writeFile = require('./classes/writeFile');
-const PdfWriter = require('./classes/PdfWrite');
+const express = require('express');
 
+const app = express();
 
-const L1 = new Reader();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('./public'));
 
-async function main () {
-    const data = await L1.read('./pessoas.csv');
-    const  processData = processFile.Process(data);
+app.get('/', (req, resp) => {
+    resp.send('Ok')
+});
 
-    const Table = new CreateTable(processData);
-
-    const html = await HTMLparser.Parser(Table);
-
-    await writeFile.Write(`${Date.now()}.html`, html);
-
-    PdfWriter.Writer(`${Date.now()}.pdf`, html);
-}
-
-main();
+app.listen(3000, () => console.log('Online'));
---
+++ process.js
@@ -0,0 +1,24 @@
+const Reader = require('./classes/Reader');
+const processFile = require('./classes/ProcessFile');
+const CreateTable = require('./classes/Table');
+const HTMLparser = require('./classes/HTMLpaser');
+const writeFile = require('./classes/writeFile');
+const PdfWriter = require('./classes/PdfWrite');
+
+
+const L1 = new Reader();
+
+async function main () {
+    const data = await L1.read('./pessoas.csv');
+    const  processData = processFile.Process(data);
+
+    const Table = new CreateTable(processData);
+
+    const html = await HTMLparser.Parser(Table);
+
+    await writeFile.Write(`${Date.now()}.html`, html);
+
+    PdfWriter.Writer(`${Date.now()}.pdf`, html);
+}
+
+main();
